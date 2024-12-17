d412aedf5b6327bc4c98eb27cb307efc71a3a920
jayascodez <jayabrooks1@gmail.com>
2024-12-17 15:40:07 +0000

    adding voting error functionality

--- src/components/VoteOnArticle.jsx
+++ src/components/VoteOnArticle.jsx
@@ -6,28 +6,29 @@ export const VoteOnArticle = ({votes, article_id}) => {
     const [userVoted, setUserVoted] = useState(false)
     const [articleLikes, setArticleLikes] = useState(votes)
     const [articleDislikes, setArticleDislikes] = useState(votes)
-
+    const [error, setError] = useState(null)
 
     const percentageArticleVotes = () => {
         const totalVotes = articleDislikes+articleLikes
         return (totalVotes === 0 ? "no votes yet" : (Math.floor((articleLikes/totalVotes)*100))+"% liked this")
     }
 
-    const handleLikes = (inc_votes) => {
+    const handleVotes = (inc_votes) => {
         setArticleLikes((currCount) => currCount + inc_votes)
         updateArticleVotes(article_id, inc_votes)
         .then((response) => {
-            setArticleLikes(response.article.votes)
+            setArticleVotes(response.article.votes)
+            if(inc_votes === 1){
+                setArticleLikes(response.article.votes)
+            }
+            if(inc_votes === -1){
+                setArticleDislikes(response.article.votes)
+            }
             setUserVoted(true)
         })
-    }
-
-    const handleDislikes = (dec_votes) => {
-        setArticleDislikes((currCount) => {currCount + dec_votes})
-        updateArticleVotes(article_id, dec_votes)
-        .then((response) => {
-            setArticleDislikes(response.article.votes)
-            setUserVoted(true)
+        .catch((err) => {
+            setUserVoted(false)
+            setError("offline -cannot vote")
         })
     }
 
@@ -42,8 +43,9 @@ export const VoteOnArticle = ({votes, article_id}) => {
     <p>Enjoying this article?</p>
     <p> {percentageArticleVotes()}</p>
     <p>Votes: {articleVotes}</p>
-    <button onClick={()=>handleLikes(1)}>Like</button>
-    <button onClick={()=> handleDislikes(-1)}>Dislike</button>   
+    <button onClick={()=>handleVotes(1)}>Like</button>
+    <button onClick={()=> handleVotes(-1)}>Dislike</button>   
+    {error && <p>{error}</p>}
     </>)
 
 
