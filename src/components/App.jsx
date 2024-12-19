f644581235e1d2c3facad54301abf5fe96f63131
jayascodez <jayabrooks1@gmail.com>
2024-12-19 15:09:27 +0000

    OR on comments and structural changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,28 @@
 import '../api'
 import '../App.css'
-import { useState } from 'react'
-import { Routes, Route, BrowserRouter } from 'react-router'
+import { Routes, Route } from 'react-router'
 import { Header } from './Header'
 import { Home } from './Home'
 import { Articles } from './Articles'
 import { AboutUs } from './AboutUs'
-import { Comments } from './Comments'
 import { SeachByTopic } from './SearchByTopic'
 import { Profile } from './Profile'
+import { SingleArticle } from './SingleArticle'
 
 function App() {
 
-  const [loading, setLoading] = useState(true)
-  const [articles, setArticles] = useState([])
-
   return (
     <>
-    <BrowserRouter>
     <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path="/articles" element={<Articles loading={loading} setLoading={setLoading} articles = {articles} setArticles={setArticles}/>}/>
-        <Route path="/articles/:article_id" element = {<Articles loading={loading} setLoading={setLoading} articles = {articles} setArticles={setArticles}/>}> Back to Articles</Route> 
-        <Route path='/articles/:article_id"/comments' element={<Comments/>}/>
-        <Route path='/search' element={<SeachByTopic articles={articles} />}/>
-        <Route path='/search/:topic' element={<SeachByTopic articles={articles} />}/>
+        <Route path="/articles" element={<Articles />}/>
+        <Route path="/articles/:article_id" element = {<SingleArticle/>}> Back to Articles</Route> 
+        <Route path='/search' element={<SeachByTopic/>}/>
+        <Route path='/search/:topic' element={<SeachByTopic />}/>
         <Route path="/about-us" element={<AboutUs/>}/>
         <Route path="/profile" element={<Profile/>}/>
       </Routes>
-    </BrowserRouter>
     </>
   )
 }
---
+++ src/components/SingleArticle.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react"
+import { Comments } from "./Comments"
+import { VoteOnArticle } from "./VoteOnArticle"
+import { useParams} from "react-router"
+import { getArticleById} from "../api"
+
+
+export const SingleArticle = () => {
+    const [singleArticle, setSingleArticle] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    const {article_id} = useParams()
+
+    useEffect(() => {
+        setLoading(true)
+            getArticleById(article_id).then((response)=> {
+            setSingleArticle(response.article)
+            setLoading(false)
+        })
+    }, [article_id])
+
+    if(loading){
+        return <p>article loading ...</p>
+    }
+
+    if(singleArticle.length != 0){
+        const convertedTime = new Date(singleArticle.created_at).toLocaleString()
+        return (<>
+            <h2>{singleArticle.title}</h2>
+            <h3>Article topic: {singleArticle.topic}</h3>
+            <h4>Written by {singleArticle.author} on {convertedTime}</h4>
+            <img src={singleArticle.article_img_url} alt={singleArticle.title} className="article-img"></img>
+            <h4>{singleArticle.body}</h4>
+            <div id="voting">
+                <VoteOnArticle votes={singleArticle.votes} article_id={singleArticle.article_id}/>
+            </div>
+            <div id="comments-box">
+                <Comments/>
+            </div>
+        </>)
+    }
+}
